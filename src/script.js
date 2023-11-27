854f4a41628108bb9408d833aefcbaa2daf8b74a
selin20862 <145472423+selin20862@users.noreply.github.com>
2023-11-27 16:29:54 +0100

    final commit

--- src/script.js
+++ src/script.js
@@ -1,41 +1,83 @@
 $( document ).ready(function() {
     
   var $grid = '';
-    
-    $('.filter button').on("click", function(){
 
-        var value = $(this).attr('data-name');
-        console.log(value);
+    var filters = {};
 
-        
-        $grid.isotope({ 
-            filter: value
+    function handleButtonClick(container, buttonClass) {
+        container.on('click', 'button', function() {
+          var $this = $(this);
+          container.find('button').removeClass('bg-purple-300');
+          $this.addClass('bg-purple-300');
+      
+          // get group key
+          var $buttonGroup = $this.parents('.' + buttonClass);
+          var filterGroup = $buttonGroup.attr('data-filter-group');
+      
+          // set filter for group
+          filters[filterGroup] = $this.attr('data-name');
+      
+          // combine filters
+          var filterValue = concatValues(filters);
+          $grid.isotope({ filter: filterValue });
+        });
+      }
+      
+      // Utiliser la fonction pour gérer les clics sur les boutons pour '.filter'
+      handleButtonClick($('.filter'), 'truc');
+      
+      // Utiliser la fonction pour gérer les clics sur les boutons pour '.genre'
+      handleButtonClick($('.genre'), 'truc');
+      
+
+// flatten object by concatting values
+function concatValues( obj ) {
+  var value = '';
+  for ( var prop in obj ) {
+    value += obj[ prop ];
+  }
+  return value;
+}
+
+$.ajax({
+    url: 'got.json',
+    method: 'GET',
+    dataType: 'json',
+    success: function (data) {
+
+        console.log(data);
+
+        var perso = "";
+        var btn = "";
+        var uniqueLastNames = [];
+
+        $.each(data, function (index, post) {
+            perso += '<div class="grid-item text-center '+post.lastName+' '+post.gender+' p-8 rounded-lg bg-amber-200 m-5">'+post.fullName+'</div>';
+            
+            // Ajouter le nom de famille à uniqueLastNames s'il n'est pas déjà présent
+            if (uniqueLastNames.indexOf(post.lastName) === -1 && post.lastName !== "") {
+                uniqueLastNames.push(post.lastName);
+            }
         });
 
-    });
+        // Créer des boutons à partir des noms de famille uniques
+        uniqueLastNames.forEach(function(lastName) {
+            btn += '<button data-name=".'+lastName+'" class="bg-red-400 hover:bg-black hover:text-red-400 text-black p-3 rounded-lg m-5">'+lastName+'</button>';
+        });
 
-    $.ajax({
-        url: 'got.json',
-        method: 'GET',
-        dataType: 'json',
-        success: function (data) {
+        $(".grid").html(perso);
+        $(".filter").append(btn);
 
-            console.log(data);
+        $grid = $('.grid').isotope({
+            itemSelector: '.grid-item',
+            layoutMode: 'fitRows',
+            getSortData: {
+                category: '[data-sort-by]'
+            }
+        });
+    }//fin success
+}); //fin ajax
 
-            var perso = "";
-            $.each(data, function (index, post) {
-
-                perso += '<div class="grid-item text-center '+post.lastName+' p-8 rounded-lg bg-amber-200 m-5">'+post.fullName+'</div>';
-
-            });
-            $(".grid").html(perso);
-
-                $grid = $('.grid').isotope({
-                itemSelector: '.grid-item',
-                layoutMode: 'fitRows'
-            });
-        }//fin success
-    }); //fin ajax
 
 
 });
